cd3813ba4f86a2802e95989fbc41bf881c4dba65
Thiago Torres <TorresmThiago@gmail.com>
2022-08-17 13:39:28 -0300

    Simple Player Moviment

---
+++ LSW-Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalLeftOffset;
+    public float horizontalRightOffset;
+    public float verticalUpOffset;
+    public float verticalDownOffset;
+
+    public float speed = 1;
+
+    private float _verticalInput;
+    private float _horizontalInput;
+
+
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
+        _horizontalInput = Input.GetAxisRaw("Horizontal");
+        _verticalInput = Input.GetAxisRaw("Vertical");
+
+        if ((_horizontalInput == 1 && transform.position.x >= horizontalRightOffset)
+          || _horizontalInput == -1 && transform.position.x <= horizontalLeftOffset)
+        {
+            _horizontalInput = 0;
+        }
+
+        if ((_verticalInput == 1 && transform.position.y >= verticalUpOffset)
+        || _verticalInput == -1 && transform.position.y <= verticalDownOffset)
+        {
+            _verticalInput = 0;
+        }
+
+        Vector3 direction = new Vector3(_horizontalInput, _verticalInput, 0);
+        transform.Translate(direction * Time.deltaTime * speed, Space.World);
+    }
+}
